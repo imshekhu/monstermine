8f6792cf38fc62c56b743b7190e6f8e9a08a6cd2
SkZ <skazmi@contractor.5amsolutions.com>
2021-10-12 14:22:56 -0500

    a

--- regulator/views.py
+++ regulator/views.py
@@ -13,7 +13,9 @@ class AccountUpdateView(View):
     def post(self, request):
         print('body -- ',request.body)
         print('dir -- ', dir(request))
-        data = json.loads(request.body.decode('utf-8'))
+        # data = json.loads(request.body.decode('utf-8'))
+        body_unicode = request.body.decode('utf-8')
+        data = json.loads(body_unicode)
         phone = data['phone']
         print(phone)
         currentcoin = data['currentcoin']
