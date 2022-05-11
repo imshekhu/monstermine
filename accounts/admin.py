9c59f1542dc588761e437e9d72bb974a84852a12
hsi-sali <sali@heidrick.com>
2022-05-11 13:39:50 -0500

    new resolved

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,14 +8,24 @@ class CustomUserAdmin(UserAdmin):
     # ...
     ordering = ('email',)
     list_display = ('email',)
-    fieldsets = (
-        (None, {
-            "fields": (
-                ('email', 'first_name', 'last_name', 'is_staff',  'password', 'profit_today', 'profit_yesterday', 'amountmined', 
-                 'watcherlink', 'binance_profile_token')
+    # fieldsets = (
+    #     (None, {
+    #         "fields": (
+    #             ('email', 'first_name', 'last_name', 'is_staff',  'password', 'profit_today', 'profit_yesterday', 'amountmined', 
+    #              'watcherlink', 'binance_profile_token', 'phone',)
                 
-            ),
-        }),
+    #         ),
+    #     }),
+    # )
+    fieldsets = (
+        (None, {"fields": (
+                ('email', 'first_name', 'last_name', 'is_staff',  'password', 'profit_today', 'profit_yesterday', 'amountmined', 
+                 'watcherlink', 'binance_profile_token',)
+                
+            ),}),
+    )
+    add_fieldsets =  (
+        (None, {'fields': ('phone', 'password')}),
     )
 
 admin.site.register(UserBase, CustomUserAdmin)
